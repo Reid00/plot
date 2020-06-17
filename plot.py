1fb4ad6d3f05753179b1e0ec3ce3f3de6a89b833
reid <isreid.zhang@outlook.com>
2020-06-17 16:06:43 +0800

    updated plot.py for some axis params

--- plot.py
+++ plot.py
@@ -1,123 +1,57 @@
-import numpy as np
+"""
+matplotlib 绘图练习
+"""
+
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
-# 折线图
-x = np.linspace(0, 2, 100)
+# %matplotlib inline
 
-plt.plot(x, x, label='linar')
-plt.plot(x, x ** 2, label='quadratic')
-plt.plot(x, x ** 3, label='cubic')
+# 设置中文字体为微软雅黑
+plt.rcParams['font.sans-serif'] = 'SimHei'
 
-plt.xlabel('x label content')
-plt.ylabel('y label content')
+def static_paint():
+        """
+        绘制静态图
+        """
+        
+        df = pd.DataFrame({
+                'city':['北京','北京','上海','上海','杭州','深圳','苏州','苏州','南京','广州'],
+                'salary':[22.5,40,14,30,25,41.5,12.5,20,16,18]
+        })
 
-plt.title('simple plot')
-plt.legend()
-plt.show()
+        fig,ax = plt.subplots(figsize=(9,6),sharey=True)
+        # sns.barplot(x='city',y='salary',data=df,ci=95,ax=ax)
+        # title 参数设置，让标题更好看
+        ax.set_title('各个城市薪资水平对比', backgroundcolor='#3c7f99',fontsize=30, weight='bold',color='white')
 
-# 散点图
+        # 由于刻度的设置是tick的属性，所以用ax.tick_param()进行设置,用参数labelsize指定刻度标签的大小，将length参数设置刻度线长短。
+        #  字体为16px大小，刻度线长度为0
+        ax.tick_params(labelsize=16,length=0)
+        plt.box(False)  #去掉四边的边框
 
-x = np.arange(0, 5, 0.2)
-# 红色破折号, 蓝色方块 ，绿色三角块
-plt.plot(x, x, 'r--', x, x ** 2, 'bs', x, x ** 3, 'g^')
+        # 对各个城市的薪水均值并从小到大排序，获取城市排序列表city_order
+        s = df.groupby(by=['city'])['salary'].agg('mean').sort_values()
+        print(s)
+        city_order = df.groupby(by=['city'])['salary'].agg('mean').sort_values().index.tolist()
+        #排序后的绘图如下
+        # Seaborn中的order和palette分别设置排列顺序和颜色。
+        sns.barplot(x='city',y='salary',data=df,ax=ax,order=city_order,palette='RdBu_r')
 
-plt.show()
+        # 在y轴上添加网格线便于观察每个柱子的数值大小,因为是在y轴上，网格线为grid，所以用ax.yaxis.grid()进行设置
+        ax.yaxis.grid(linewidth=0.5,color='black') # 设置y 轴网格线
+        ax.set_axisbelow(True)         # 将网格线置于底层
+        # 由于x轴和y轴含义比较清晰，所以可以将横纵坐标的标签去掉，同时，为了更直观，可以将y轴的刻度标签由20，15...换成20k,15k...
+        ax.set_xlabel('')
+        ax.set_ylabel('')
+        # 将0处设为空字符串，其他地方加上k
+        ax.set_yticklabels(['','5k','10k','15k','20k','35k','30k','35k'])
+        # x轴标签更改
+        plt.xticks(rotation='45', color='red',fontsize=10)
+        plt.subplots_adjust(bottom=0.2)  #因为竖着字太长，生成图片中的x轴标签会被截取。因此设置距离底部0.2
+        # plt.show()
+        
 
-# 直方图
-np.random.seed(19680801)
-mu1, singma1 = 100, 15
-mu2, singma2 = 80, 15
-x1 = mu1 + singma1 * np.random.randn(10000)
-x2 = mu2 + singma2 * np.random.randn(10000)
-# the histogram of the data
-# 50：将数据分成50组
-# facecolor：颜色；alpha：透明度
-# density：是密度而不是具体数值
-n1, bins1, patches1 = plt.hist(x1, 50, density=True, facecolor='g', alpha=1)
-n2, bins2, patches2 = plt.hist(x2, 50, density=True, facecolor='r', alpha=0.2)
-
-# n：概率值；bins：具体数值；patches：直方图对象。
-plt.xlabel('Smarts')
-plt.ylabel('Probability')
-plt.title('Histogram of IQ')
-
-plt.text(110, 0.25, r'$\mu=100,\ \sigma=15$')
-plt.text(50, 0.25, r'$\mu=80,\ \sigma=15$')
-
-# 设置x，y轴的具体范围
-plt.axis([40, 160, 0, 0.03])
-plt.grid(True)
-plt.show()
-
-# 并列柱状图
-size = 5
-a = np.random.random(size)
-b = np.random.random(size)
-c = np.random.random(size)
-x = np.arange(size)
-# 有多少个类型，只需更改n即可
-total_width, n = 0.8, 3
-width = total_width / n
-# 重新拟定x的坐标
-x = x - (total_width - width) / 2
-# 这里使用的是偏移
-plt.bar(x, a, width=width, label='a')
-plt.bar(x + width, b, width=width, label='b')
-plt.bar(x + 2 * width, c, width=width, label='c')
-
-plt.legend()
-plt.show()
-
-# 叠加柱状图
-size = 5
-a = np.random.random(size)
-b = np.random.random(size)
-c = np.random.random(size)
-
-x = np.arange(size)
-
-# 这里使用的是偏移
-plt.bar(x, a, width=0.5, label='a', fc='r')
-plt.bar(x, b, bottom=a, width=0.5, label='b', fc='g')
-plt.bar(x, c, bottom=a + b, width=0.5, label='c', fc='b')
-
-plt.ylim(0, 2.5)
-plt.legend()
-plt.grid(True)
-plt.show()
-
-# 普通饼图
-
-labels = ['Frogs', 'Hogs', 'Dogs', 'Logs']
-size = [15, 30, 45, 10]
-# 设置分离的距离，0表示不分离
-explode = (0, 0.1, 0, 0)
-plt.pie(size, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-# Equal aspect ratio 保证画出的图是正圆形
-plt.axis('equal')
-plt.show()
-
-# 嵌套饼图
-# 设置每环的宽度
-size = 0.3
-vals = np.array([[60., 32.], [37., 40.], [29., 10.]])
-
-# 通过get_cmap随机获取颜色
-cmap = plt.get_cmap("tab20c")
-outer_colors = cmap(np.arange(3) * 4)
-inner_colors = cmap(np.array([1, 2, 5, 6, 9, 10]))
-
-print(vals.sum(axis=1))
-# [92. 77. 39.]
-
-plt.pie(vals.sum(axis=1), radius=1, colors=outer_colors,
-        wedgeprops=dict(width=size, edgecolor='w'))
-print(vals.flatten())
-# [60. 32. 37. 40. 29. 10.]
-
-plt.pie(vals.flatten(), radius=1 - size, colors=inner_colors,
-        wedgeprops=dict(width=size, edgecolor='w'))
-
-# equal 使得为正圆
-plt.axis('equal')
-plt.show()
+if __name__ == "__main__":
+    static_paint()
